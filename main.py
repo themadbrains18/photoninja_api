b3ead469ac52c053ec9ca36d1a7665151ea28086
amit-mehta28 <amitmehta7973@gmail.com>
2024-03-07 18:48:54 +0530

    session redis update and delete images after 1 hour

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from flask_cors import CORS
 from flask_session import Session
 import os
 import secrets
+from datetime import datetime, timedelta
+import glob
+import redis
 
 from App.Features.bg_remove import bg_remove_route
 from App.Features.add_bg import add_bg_route
@@ -21,12 +24,12 @@ app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'uploads')
 # Set the secret key for the Flask app
 app.config['SECRET_KEY'] = secrets.token_hex(32)
 
-# Set the session type to filesystem
-app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SESSION_FILE_DIR'] = os.path.join(app.root_path, 'session_files')
+app.config['SESSION_TYPE'] = 'redis'
+app.config['SESSION_REDIS'] = redis.from_url('redis://default:7035658f8b314deba49306cbf7821d5e@relaxed-gelding-41419.upstash.io:41419')
+
 
 # Ensure the directory for storing sessions exists
-os.makedirs(app.config['SESSION_FILE_DIR'], exist_ok=True)
+# os.makedirs(app.config['SESSION_FILE_DIR'], exist_ok=True)
 
 # Initialize Flask-Session
 server_session = Session(app)
@@ -54,6 +57,21 @@ compressing(app)
 image_enhance_route(app)
 image_convertor_route(app)
 
+
+def delete_old_files(folder_path, max_age_hours=1):
+    current_time = datetime.now()
+    max_age = timedelta(hours=max_age_hours)
+
+    for file_path in glob.glob(os.path.join(folder_path, '*')):
+        file_creation_time = datetime.fromtimestamp(os.path.getctime(file_path))
+        if current_time - file_creation_time > max_age:
+            os.remove(file_path)
+            print(f"Deleted: {file_path}")
+
+
+# Run the deletion for both folders
+delete_old_files(os.path.join(os.getcwd(), 'static'))
+delete_old_files(os.path.join(os.getcwd(), 'uploads'))
 
 
 if __name__ == '__main__':
