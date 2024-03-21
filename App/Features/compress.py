b162a9d6db4614983903ba2475b0f8d9ed5f71b3
amit-mehta28 <amitmehta7973@gmail.com>
2024-03-21 14:18:10 +0530

    compress module session removed

--- App/Features/compress.py
+++ App/Features/compress.py
@@ -12,63 +12,59 @@ UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def timestampsWithFilename(filename):
+def generate_timestamped_filename(filename):
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    return f"static/{filename}_{timestamp}_tmb.png"
+    return f"{filename}_{timestamp}.jpg"
 
 def compress_route(app):
     @app.route('/api/upload-compress', methods=['POST'])
     @cross_origin(supports_credentials=True)
     def upload_file_for_compress():
-        if request.method == 'POST':
-            if 'file' not in request.files:
-                return jsonify({'error': 'No file part'})
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file part'})
 
-            file = request.files['file']
-            if file.filename == '' or not allowed_file(file.filename):
-                return jsonify({'error': 'Invalid file'})
-            
-            filename = secure_filename(file.filename)
-            file_path = os.path.join(UPLOAD_FOLDER, filename)
-            file.save(file_path)
-            
-            print("session store before")
-            session['original_img_for_compression'] = file_path
-            print("session store after")
-            return jsonify({'filename': filename})
+        file = request.files['file']
+        if file.filename == '' or not allowed_file(file.filename):
+            return jsonify({'error': 'Invalid file'})
 
-        return jsonify({'error': 'Invalid request'})
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        return jsonify({'filename': filename})
 
 def compressing(app):
     @app.route('/api/compressing', methods=['POST'])
     @cross_origin(supports_credentials=True)
     def compressing_file():
         quality = request.form.get('quality')
+        file = request.files.get('file')
 
-        session_file_path = session.get('original_img_for_compression')
-        print(session_file_path,"sfajksdfhlsdfhlk")
-        if session_file_path:
-            original_image = cv2.imread(session_file_path, cv2.IMREAD_COLOR)
+        if not file:
+            return jsonify({'error': 'File not provided'})
 
-            if original_image is not None:
-                jpeg_quality = int(quality) if quality.isdigit() else 60
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
-                # Generate timestamp
-                timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-                filename = os.path.basename(session_file_path)
+        file.save(file_path)
 
-                # Modify the file name with timestamp
-                compressed_filename = f"{filename}_{timestamp}.jpg"
+        if not os.path.exists(file_path):
+            return jsonify({'error': 'File not found'})
 
-                # Define the path for the compressed image in the 'static' folder
-                compressed_file_path = os.path.join('static', compressed_filename)
+        original_image = cv2.imread(file_path, cv2.IMREAD_COLOR)
 
-                # Compress and save the image
-                cv2.imwrite(compressed_file_path, original_image, [cv2.IMWRITE_JPEG_QUALITY, jpeg_quality])
+        if original_image is not None:
+            jpeg_quality = int(quality) if quality.isdigit() else 60
 
-                # Return response with filename and quality
-                return jsonify({'filename': compressed_file_path, 'quality': jpeg_quality})
-            else:
-                return jsonify({'error': 'Failed to read the original image'})
+            compressed_filename = generate_timestamped_filename(filename)
+            compressed_file_path = os.path.join('static', compressed_filename)
 
-        return jsonify({'error': 'Original image path not found in session'})
+            cv2.imwrite(compressed_file_path, original_image, [cv2.IMWRITE_JPEG_QUALITY, jpeg_quality])
+
+            return jsonify({'filename': compressed_file_path, 'quality': jpeg_quality})
+        else:
+            return jsonify({'error': 'Failed to read the original image'})
+
+
+
+
